b8f00427c943588f4c073aa7e835050af4869770
“pdubuc” <pd21248@gmail.com>
2024-01-14 18:42:41 -0800

    initial commit

---
+++ src/components/Users.js
@@ -0,0 +1,79 @@
+import { Component } from "react";
+import User from "./User";
+
+import classes from "./Users.module.css";
+
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+      more: "Test",
+    };
+  }
+
+  componentDidUpdate() {
+    // try catch is not an option because that only works w/ regular JS code, not jsx
+    // try {
+    //   someCodeWhichMightFail()
+    // } catch (err) {
+    //   // handle error
+    // }
+    if (this.props.users.length === 0) {
+      throw new Error("No users provided!");
+    }
+  }
+
+  toggleUsersHandler() {
+    // this.state.showUsers = false; --> this is NOT how to do it!
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={this.toggleUsersHandler.bind(this)}>
+//         {this.state.showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+//       {this.state.showUsers && usersList}
+//     </div>
+//   );
+// };
+
+export default Users;
